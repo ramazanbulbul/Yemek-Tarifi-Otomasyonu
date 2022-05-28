031b275e7bd990f05736ef5dde1d110c94d3a8c1
ramazanbulbul <59332779+ramazanbulbul@users.noreply.github.com>
2022-05-28 17:52:38 +0300

    Create module-info.java

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module RamazanBulbul_202503308 {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires javafx.base;
+	requires java.sql;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
